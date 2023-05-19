e277b9374b5f2ce9b6432641fb7abd9fb65ad423
wambui <wambui@wambui.wambui>
2023-05-19 14:51:57 +0300

    small changes

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -20,35 +20,35 @@ class ProductSeeder extends Seeder
             'price'=>'900',
             'description'=>"A smartphone with 4gb ram and nice camera",
             'category'=>"Mobilephones",
-            'gallery'=>""
+            'gallery'=>"https://m.media-amazon.com/images/I/8104evx11QL._SX385_.jpg"
             ],
             [
             'name'=>'Samsung A22',
             'price'=>'900',
             'description'=>"A smartphone with 8gb ram and nice security features",
             'category'=>"Mobilephones",
-            'gallery'=>""
+            'gallery'=>"https://www.smartprix.com/bytes/wp-content/uploads/2021/08/Samsung-A22-5G-review-with-pros-cons-0-scaled.jpg"
             ],
             [
             'name'=>'Sony bravia',
             'price'=>'1400',
             'description'=>"A smart tv with  much more features",
             'category'=>"tv",
-            'gallery'=>""
+            'gallery'=>"https://ke.jumia.is/unsafe/fit-in/300x300/filters:fill(white)/product/66/93916/1.jpg?7996"
             ],
             [
             'name'=>'Samsung tv',
             'price'=>'1300',
             'description'=>"Fullt hd abdroid tv",
             'category'=>"tv",
-            'gallery'=>""
+            'gallery'=>"https://media.4rgos.it/i/Argos/9336397_R_Z001A"
             ],
             [
             'name'=>'LG fridge',
             'price'=>'4000',
             'description'=>"A smart fridge much more features",
             'category'=>"Fridges",
-            'gallery'=>""
+            'gallery'=>"https://cdnprod.mafretailproxy.com/sys-master-root/h69/he2/48830818320414/1700Wx1700H_157206_Main.jpg"
             ]
             
         ]);
