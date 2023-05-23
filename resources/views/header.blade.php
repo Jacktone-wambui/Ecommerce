d837427db56cfdaaa8cd08dac1ce788b83896302
wambui <wambui@wambui.wambui>
2023-05-23 07:58:16 +0300

    ss

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -17,7 +17,7 @@ if(Session::has('user'))
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="/">SokoLetu</a>
+      <a class="navbar-brand" href="/"><strong>SokoLetu</strong></a>
     </div>
 
     <!-- Collect the nav links, forms, and other content for toggling -->
